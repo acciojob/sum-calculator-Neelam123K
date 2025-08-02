832b17429561459977994bf4b4653f334dbd0c45
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-08-02 14:25:02 +0530

    test

--- src/components/SumCalculator.js
+++ src/components/SumCalculator.js
@@ -1,30 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 const SumCalculator = () => {
-    const [numbers, setNumbers] = useState([]);
-    const [sum, setSum] = useState(0);
+    // const [numbers, setNumbers] = useState([]);
+    // const [sum, setSum] = useState(0);
 
-    const handleInputChange = (event) => {
-        const value = event.target.value;
-        if (!isNaN(value) && value !== '') {
-            setNumbers(prevNumbers => [...prevNumbers, parseInt(value)]);
-        }
-    };
+    // const handleInputChange = (event) => {
+    //     const value = event.target.value;
+    //     if (!isNaN(value) && value !== '') {
+    //         setNumbers(prevNumbers => [...prevNumbers, parseInt(value)]);
+    //     }
+    // };
 
-    useEffect(() => {
-        const calculateSum = async () => {
-            const total = numbers.reduce((acc, num) => acc + num, 0);
-            setSum(total);
-        };
+    // useEffect(() => {
+    //     const calculateSum = async () => {
+    //         const total = numbers.reduce((acc, num) => acc + num, 0);
+    //         setSum(total);
+    //     };
 
-        calculateSum();
-    }, [numbers]);
+    //     calculateSum();
+    // }, [numbers]);
 
     return (
         <div>
             <h1>Sum Calculator</h1>
-            <input type="number" onChange={handleInputChange} />
-            <h2>Current Sum: {sum}</h2>
+            {/* <input type="number" onChange={handleInputChange} />
+            <h2>Current Sum: {sum}</h2> */}
         </div>
     );
 };
